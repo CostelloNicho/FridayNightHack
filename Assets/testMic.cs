9a846123ad92c98426fe8c7aa84a62aa2c200d7c
CostelloNicho <nicholasjcostello@gmail.com>
2016-11-21 16:47:13 -0500

    mic test

---
+++ Assets/testMic.cs
@@ -0,0 +1,35 @@
+﻿#region Directives
+
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+#endregion
+
+public class testMic : MonoBehaviour
+{
+    public string Device;
+
+    public AudioClip StreamClip;
+
+    public AudioSource Src;
+
+    // Use this for initialization
+    IEnumerator Start()
+    {
+
+        foreach (var device in Microphone.devices.Where(device => device.Contains("Yeti")))
+        {
+            Device = device;
+            break;
+        }
+
+        Src.clip = Microphone.Start(deviceName: Device, loop: true, lengthSec: 30,
+            frequency: 44100);
+
+        yield return new WaitForEndOfFrame();
+       
+        Src.Play();
+    }
+
+}
